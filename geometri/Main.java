1f827a45e8b89e27ba02d662a33d47da23ae66c4
Syauqiqh <shauqikhairi@gmail.com>
2023-06-13 08:59:45 +0800

    membuat class main dan menjalankan program

---
+++ geometri/Main.java
@@ -0,0 +1,14 @@
+
+package geometri;
+
+public class Main {
+    public static void main(String[] args) {
+     Lingkaran syauqi = new Lingkaran (8);
+        System.out.println("luas lingkaran : "+syauqi.luas());
+        System.out.println("keliling lingkaran : "+syauqi.keliling());
+      PersegiPanjang syauqikh = new PersegiPanjang(2,3);
+       System.out.println("luas PersegiPanjang : "+syauqikh.luas());
+        System.out.println("keliling PersegiPanjang: "+syauqikh.keliling());
+    }
+   
+}
